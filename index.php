4038077cb72d4836fd5efb5ad0d849b108ac9554
Tim Zagelow <timzagelow@gmail.com>
2017-02-11 12:16:06 -0800

    pagination setup

--- index.php
+++ index.php
@@ -38,13 +38,15 @@
 function tableHandler(table, options) {
 	var that = this,
 	defaults = {
-		totalRowCount: 0,
-		rowsPerPage: 25,
+		sortCol: 0,
+		sortDir: 'asc',
 		currentPage: 1,
-		sortCol: 0
+		rowsPerPage: 25
 	};
 	
 	that.options = that.mergeOptions(defaults, options);
+	that.totalRowCount = 0;
+	that.paginationContainer = null;
 	that.table = table;
 	that.init();
 }
@@ -53,30 +55,120 @@ tableHandler.prototype = {
 	init: function() {
 		this.totalRowCount = this.getTotalRowCount();
 		this.addSortHandlers();
+		this.initPagination();
 		this.showCurrentPage();
 	},
 	
 	showCurrentPage: function() {
 		var that = this,
-				tbody = that.table.tBodies[0], 
-				rows = tbody.rows,
-				start = (that.options.currentPage - 1) * that.options.rowsPerPage,
-				end = start + (that.options.rowsPerPage - 1);
+				col = that.options.sortCol;
 		
-		for (var i=0; i < rows.length; i++) {
-			if (i < start || i > end) {
-				// rows[i].style.visibility = 'hidden';
-				// rows[i].style.height = '0px';
-			} else {
-				// rows[i].style.display = 'table-row';
-			}
-		}
+		that.sortRows(col, that.getSortType(col), that.options.sortDir);
+		that.showPagination();
 	},
 	
+	getSortType: function(col) {
+		var that = this,
+				headers = that.table.querySelectorAll('th'),
+				type = 'str';
+		
+		for (var i=0; i < headers.length; i++) {
+			if (col === i) {
+				 type = headers[i].getAttribute('data-col-type');
+			}
+		}
+		
+		return type;
+	},
+	
+	initPagination: function() {
+		var that = this;
+	
+		that.paginationContainer = document.createElement('div');
+		that.paginationContainer.className = 'pagination-container';
+		that.table.parentNode.insertBefore(that.paginationContainer, that.table.nextSibling);
+	},
+	
+	getPagination: function() {
+		var that = this,
+				start = (that.options.currentPage - 1) * that.options.rowsPerPage,
+				end = start + (that.options.rowsPerPage - 1),
+				numPages = Math.ceil(that.totalRowCount / that.options.rowsPerPage);
+	
+		return {
+			start: start,
+			end: end,
+			pages: numPages
+		};
+	},
+	
+	showPagination: function() {
+		var that = this,
+				p = that.getPagination(),
+				prevButton = document.createElement('a'),
+				nextButton = document.createElement('a'),
+				resultCounts = document.createElement('span'),
+				resultText,
+				endCount;
+				
+		if ((p.end + 1) > that.totalRowCount) {
+			endCount = that.totalRowCount;
+		} else {
+			endCount = p.end + 1;
+		}
+		
+		resultText = (p.start + 1) + " &ndash; " + endCount;
+		resultText += " of " + that.totalRowCount + " results found";
+		
+		resultCounts.innerHTML = resultText;
+		prevButton.innerHTML = 'Previous';
+		nextButton.innerHTML = 'Next';
+
+		prevButton.onclick = function() {
+			return function(e) {
+				that.prevPage(e);
+			}
+		}();
+	
+		nextButton.onclick = function() {
+			return function(e) {
+				that.nextPage(e);
+			}
+		}();
+		
+		prevButton.style.visibility = that.options.currentPage === 1 ? 'hidden' : 'visible';
+		
+		if ((that.options.currentPage * that.options.rowsPerPage) > that.totalRowCount) {
+			nextButton.style.visibility = 'hidden';
+		} else {
+			nextButton.style.visibility = 'visible';
+		}
+		
+		// remove all children in the pagination container
+		while (that.paginationContainer.firstChild) {
+			that.paginationContainer.removeChild(that.paginationContainer.firstChild);
+		}
+		
+		that.paginationContainer.appendChild(prevButton);
+		that.paginationContainer.appendChild(resultCounts);	
+		that.paginationContainer.appendChild(nextButton);	
+	},
+	
+	prevPage: function() {
+		var that = this;
+		that.options.currentPage -= 1;
+		that.showCurrentPage();
+	},
+
+	nextPage: function() {
+		var that = this;
+		that.options.currentPage += 1;
+		that.showCurrentPage();
+	},
+		
 	addSortHandlers: function() {
 		var that = this;
 		var headers = that.table.querySelectorAll('th');
-		console.log(headers);
 		
 		for (var j=0; j < headers.length; j++) {
 			// add event listener for click with closure to pass the table
@@ -94,9 +186,11 @@ tableHandler.prototype = {
 				sortCol = e.target.cellIndex;
 				newSortDir = that.getNewSortDir(sortCol);
 		
-		if (typeof sortBy !== 'undefined') {	
+		if (typeof sortBy !== 'undefined') {
+			that.options.currentPage = 1;	
 			that.sortRows(sortCol, sortBy, newSortDir);
 			that.setSortDir(sortCol, newSortDir);
+			that.showCurrentPage();
 		}
 		
 	}, 	
@@ -106,24 +200,20 @@ tableHandler.prototype = {
 				ths = that.table.getElementsByTagName('th');
 		
 		if (that.options.sortCol !== col) {
-			return 'asc';
+			that.options.sortDir = 'asc';
+		} else if (that.options.sortDir === 'desc') {
+			that.options.sortDir = 'asc';
+		} else if (that.options.sortDir === 'asc') {
+			that.options.sortDir = 'desc';
 		}
 		
-		var dir = ths[that.options.sortCol].getAttribute('data-dir');
-		
-		if (dir === null) {
-			return 'asc';
-		} else if (dir === 'desc') {
-			return 'asc';
-		} else {
-			return 'desc';
-		}
+		return that.options.sortDir;
 	},
 
 	setSortDir: function(col, dir) {
 			var that = this,
 					ths = that.table.getElementsByTagName('th');
-
+			
 			// remove 'sort-col' class from all THs		
 			for (var i=0; i < ths.length; i++) {
 				ths[i].removeAttribute('data-dir');
@@ -142,7 +232,7 @@ tableHandler.prototype = {
 				tbody = that.table.tBodies[0],
 				rows = [],
 				value;
-	
+		
 		for (i=0; i < tbody.rows.length; i++) {
 			value = tbody.rows[i].cells[index].innerHTML;
 			rows.push({
@@ -185,11 +275,18 @@ tableHandler.prototype = {
 			tbody.deleteRow(0);
 		}
 	
+		var pagination = that.getPagination();
+		
 		for (var i in rows) {
+			if (i < pagination.start || i > pagination.end) {
+				rows[i].row.style.display = 'none';
+			}  else {
+				rows[i].row.style.display = 'table-row';
+			}
+			
 			tbody.appendChild(rows[i].row);
 		}	
 	},
-	
 	
 	getTotalRowCount: function() {
 			var that = this,
@@ -213,7 +310,6 @@ for (var i=0; i < tables.length; i++) {
 		rowsPerPage: 2
 	};
 	var myTable = new tableHandler(tables[i], defaults);
-	console.log(myTable);
 }
 
 </script>
