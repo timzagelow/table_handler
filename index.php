c4bf0268a38443890306412520daabcec05d3ee2
Tim Zagelow <timzagelow@gmail.com>
2017-02-11 16:02:47 -0800

    inject sort icon

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script src="https://use.fontawesome.com/25bb81ef1d.js"></script>
 <style>
 	table {
 		padding: 8px;
@@ -7,41 +8,65 @@
 	th, td {
 		padding: 8px 12px;
 	}
+	.tabl-sort-icon {
+		padding-left: 6px;
+	}
+	
+	.tabl-wrapper table thead th {
+		cursor: default;
+	}
 </style>
-
+<div class="tabl-wrapper">
 <table class="sorter" data-rows-per-page="2">
 <thead>
 	<th data-col-type="int" data-dir="asc"">Column 1</th>
 	<th data-col-type="str">Column 2</th>
 	<th data-col-type="date">Column 3</th>
+	<th data-col-type="str">Column 4</th>
 </thead>
 <tbody>
 	<tr>
 		<td>123</td>
 		<td>hello</td>
 		<td>2014-01-03</td>
+		<td>hey</td>
 	</tr>
 	<tr>
 		<td>321</td>
 		<td>pff</td>
 		<td>2013-02-03</td>
+		<td>hey</td>
 	</tr>
 	<tr>
 		<td>432</td>
 		<td>ola</td>
 		<td>2017-02-03</td>
+		<td>hey</td>
 	</tr>
 </tbody>
 </table>
-
+</div>
 <script>
-function tableHandler(table, options) {
+function Tabl(table, options) {
 	var that = this,
 	defaults = {
 		sortCol: 0,
 		sortDir: 'asc',
 		currentPage: 1,
-		rowsPerPage: 25
+		rowsPerPage: 25,
+		previousText: 'Previous',
+		nextText: 'Next',
+		classes: {
+			resultContainer: 'tabl-result-container',
+			resultCount: 'tabl-result-count',
+			prevButton: 'tabl-prev-button',
+			nextButton: 'tabl-next-button',
+			sortIcon: 'tabl-sort-icon',
+			icons: {
+				sortAsc: 'fa fa-caret-up',
+				sortDesc: 'fa fa-caret-down'
+			},
+		}
 	};
 	
 	that.options = that.mergeOptions(defaults, options);
@@ -51,7 +76,7 @@ function tableHandler(table, options) {
 	that.init();
 }
 
-tableHandler.prototype = {
+Tabl.prototype = {
 	init: function() {
 		this.totalRowCount = this.getTotalRowCount();
 		this.addSortHandlers();
@@ -85,7 +110,7 @@ tableHandler.prototype = {
 		var that = this;
 	
 		that.paginationContainer = document.createElement('div');
-		that.paginationContainer.className = 'pagination-container';
+		that.paginationContainer.className = that.options.classes.resultContainer;
 		that.table.parentNode.insertBefore(that.paginationContainer, that.table.nextSibling);
 	},
 	
@@ -120,9 +145,13 @@ tableHandler.prototype = {
 		resultText = (p.start + 1) + " &ndash; " + endCount;
 		resultText += " of " + that.totalRowCount + " results found";
 		
+		resultCounts.className = that.options.classes.resultCount;
+		prevButton.className = that.options.classes.prevButton;
+		nextButton.className = that.options.classes.nextButton;
+		
 		resultCounts.innerHTML = resultText;
-		prevButton.innerHTML = 'Previous';
-		nextButton.innerHTML = 'Next';
+		prevButton.innerHTML = that.options.previousText;
+		nextButton.innerHTML = that.options.nextText;
 
 		prevButton.onclick = function() {
 			return function(e) {
@@ -170,30 +199,70 @@ tableHandler.prototype = {
 		var that = this;
 		var headers = that.table.querySelectorAll('th');
 		
-		for (var j=0; j < headers.length; j++) {
+		for (var i=0; i < headers.length; i++) {
 			// add event listener for click with closure to pass the table
-			headers[j].onclick = function(table) {
-   			return function(event) {
-        	that.handleSortHeaderClick(event, table);
-    		}
+			headers[i].onclick = function(table) {
+   				return function(event) {
+        			that.handleSortHeaderClick(event, table);
+    			}
 			}(this.table);
 		}
 	},
 
 	handleSortHeaderClick: function(e, table) {
 		var that = this,
-				sortBy = e.target.getAttribute('data-col-type'),
-				sortCol = e.target.cellIndex;
-				newSortDir = that.getNewSortDir(sortCol);
+			sortBy = e.target.getAttribute('data-col-type'),
+			sortCol = e.target.cellIndex;
+			newSortDir = that.getNewSortDir(sortCol);
 		
 		if (typeof sortBy !== 'undefined') {
 			that.options.currentPage = 1;	
-			that.sortRows(sortCol, sortBy, newSortDir);
 			that.setSortDir(sortCol, newSortDir);
+			that.addSortHeaderIcon(sortCol, newSortDir);
+			that.sortRows(sortCol, sortBy, newSortDir);
 			that.showCurrentPage();
 		}
-		
 	}, 	
+	
+	addSortHeaderIcon: function(col, dir) {
+		var that = this,
+			headers = that.table.querySelectorAll('th'),
+			iconContainer = document.createElement('span'),
+			icon = document.createElement('i');
+		
+		iconContainer.className = that.options.classes.sortIcon;
+		
+		icon.className = dir === 'asc' ? 
+			that.options.classes.icons.sortAsc : 
+			that.options.classes.icons.sortDesc;
+		
+		iconContainer.appendChild(icon);
+		
+		// allows the click handler function for the th to be called
+		iconContainer.style.pointerEvents = 'none';
+		
+		that.clearSortHeaderIcons();
+		
+		for (var i=0; i < headers.length; i++) {
+			if (i === col) {
+				headers[i].appendChild(iconContainer);
+			}
+		}
+
+	},
+	
+	clearSortHeaderIcons: function() {
+		var that = this,
+			headers = that.table.querySelectorAll('th');
+			
+		for (var i=0; i < headers.length; i++) {
+			for (var j in headers[i].children) {
+				if (headers[i].children[j].className === that.options.classes.sortIcon) {
+					headers[i].children[j].parentNode.removeChild(headers[i].children[j]);
+				}
+			}
+		}
+	},
 	
 	getNewSortDir: function(col) {
 		var that = this,
@@ -212,7 +281,7 @@ tableHandler.prototype = {
 
 	setSortDir: function(col, dir) {
 			var that = this,
-					ths = that.table.getElementsByTagName('th');
+				ths = that.table.getElementsByTagName('th');
 			
 			// remove 'sort-col' class from all THs		
 			for (var i=0; i < ths.length; i++) {
@@ -232,7 +301,7 @@ tableHandler.prototype = {
 				tbody = that.table.tBodies[0],
 				rows = [],
 				value;
-		
+			
 		for (i=0; i < tbody.rows.length; i++) {
 			value = tbody.rows[i].cells[index].innerHTML;
 			rows.push({
@@ -309,7 +378,7 @@ for (var i=0; i < tables.length; i++) {
 	var defaults = {
 		rowsPerPage: 2
 	};
-	var myTable = new tableHandler(tables[i], defaults);
+	var myTable = new Tabl(tables[i], defaults);
 }
 
 </script>
